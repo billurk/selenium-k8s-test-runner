355b1e9df5f3c8d063bd2742d346279465c236f1
Billur Kaymak <billur.kaymak@payten.com>
2026-01-22 14:58:24 +0300

    test project

---
+++ tests/test_google.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def test_google_title():
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+
+    driver = webdriver.Remote(
+        command_executor="http://selenium-hub:4444/wd/hub",
+        options=options
+    )
+
+    driver.get("https://www.google.com")
+    assert "Google" in driver.title
+    driver.quit()
